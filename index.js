38ec4015392fbc4eabbeb99a59e35e96d1d9e0a2
Yasushi Ikeda <yikeda6616@gmail.com>
2019-04-09 14:23:37 -0700

    basic scaffolding

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('Hello World');
+/*
+ * Primary file for the API
+ *
+ */
+
+// Dependencies
+const http = require('http');
+
+// The server should respond to all requests with a string
+const server = http.createServer((req, res, next) => {
+  res.end('Hello World\n');
+});
+
+// Start the server, and have it listen on PORT 3000
+server.listen(3000, () => {
+  console.log('The server is listening on PORT 3000');
+});

99a03c3ab5ce2bac6e28cc3b283f89bbe43cd763
Yasushi Ikeda <yikeda6616@gmail.com>
2019-04-09 21:21:30 -0700

    Parsing headers

--- index.js
+++ index.js
@@ -11,20 +11,26 @@ const url = require('url');
 const server = http.createServer((req, res, next) => {
   // Get the URL and parse it
   const parsedUrl = url.parse(req.url, true);
+
   // Get the path
   const path = parsedUrl.pathname;
   const trimmedPath = path.replace(/^\/+|\/+$/g, ''); // trim final slash
 
+  // Get the query string as an object
+  const queryStringObject = parsedUrl.query;
+  console.log(queryStringObject);
+
   // Get the HTTP Method
   const method = req.method.toLowerCase();
+
+  // Get the headers as an object
+  const headers = req.headers;
 
   // Send the response
   res.end('Hello World\n');
 
   // Log the request path
-  console.log(
-    `Request received on path: ${trimmedPath} with method: ${method}`
-  );
+  console.log(`Request received with these headers:${headers}`);
 });
 
 // Start the server, and have it listen on PORT 3000

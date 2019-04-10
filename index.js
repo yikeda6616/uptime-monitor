04acae23af35845b02879c85e3b98359a30ee263
Yasushi Ikeda <yikeda6616@gmail.com>
2019-04-09 22:56:21 -0700

    Parsing Payloads

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
 // Dependencies
 const http = require('http');
 const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
 
 // The server should respond to all requests with a string
 const server = http.createServer((req, res, next) => {
@@ -20,17 +21,23 @@ const server = http.createServer((req, res, next) => {
   const queryStringObject = parsedUrl.query;
   console.log(queryStringObject);
 
-  // Get the HTTP Method
-  const method = req.method.toLowerCase();
+  const method = req.method.toLowerCase(); // Get the HTTP Method
 
-  // Get the headers as an object
-  const headers = req.headers;
+  const headers = req.headers; // Get the headers as an object
 
-  // Send the response
-  res.end('Hello World\n');
+  // Get the payload, if any
+  const decoder = new StringDecoder('utf-8');
+  let buffer = '';
+  req.on('data', data => (buffer += decoder.write(data)));
+  req.on('end', () => {
+    buffer += decoder.end();
 
-  // Log the request path
-  console.log(`Request received with these headers:${headers}`);
+    // Send the response
+    res.end('Hello World\n');
+
+    // Log the request path
+    console.log(`Request received with this payload: ${buffer}`);
+  });
 });
 
 // Start the server, and have it listen on PORT 3000
